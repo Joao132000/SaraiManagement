7520272cc649bbf60af308750928bb28c86fbb0a
Matheus Fidelis <matheusfidelis@yahoo.com>
2021-05-06 20:18:16 -0300

    Classes iniciais

---
+++ SaraiManagement/Models/Doador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaraiManagement.Models
+{
+    public class Doador
+    {
+        public int DoadorID { get; set; }
+        public string Nome { get; set; }
+        public string Endereço { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public Enum regularidadeDoador { get; set; }
+        public DateTime inicioDaDoacao { get; set; }
+    }
+}

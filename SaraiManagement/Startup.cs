99d02e0a5bc7fcbc188d0a380ff8dab37e85460a
Davi_Nascimento <davi-ds15@hotmail.com>
2021-05-10 20:04:54 -0300

    Startup

--- SaraiManagement/Startup.cs
+++ SaraiManagement/Startup.cs
@@ -8,26 +8,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SaraiManagement.Models;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace SaraiManagement
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
+        ///Davi
+        public Startup(IConfiguration configuration) => Configuration = configuration; 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            //services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:SaraiManagement:ConnectionString"]));
+            //services.AddTransient<IAlunoRepositorio, EFAlunoRepositorio>();
+            //services.AddTransient<IDoacaoRepositorio, EFDoacaoRepositorio>();
+            //services.AddTransient<IDonatarioRepositorio, EFDonatarioRepositorio>();
+            //services.AddTransient<IDoadorRepositorio, EFDoadorRepositorio>();
+            //services.AddTransient<ICaixaRepositorio, EFCaixaRepositorio>();
+            //services.AddTransient<IMovimentacaoRepositorio, EFMovimentacaoRepositorio>();
+            //services.AddTransient<IUsuarioRepositorio, EFUsuarioRepositorio>();
+            //services.AddTransient<IItemDoadoRepositorio, EFItemDoadoRepositorio>();
             services.AddMvc();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -37,23 +42,22 @@ namespace SaraiManagement
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+            app.UseStatusCodePages();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(name: "default", pattern: "{controller}/{action}/{id?}", defaults: new { controller = "Index", action = "Index" });
             });
+            //SeedData.EnsurePopulated(app);
+
+           //Davi
         }
     }
 }

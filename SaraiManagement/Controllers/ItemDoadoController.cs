7c6509e7a6c9fc294ad7a223fedebf7d45638391
Matheus Fidelis <matheusfidelis@yahoo.com>
2021-05-10 20:22:24 -0300

    Criacao Controllers

--- SaraiManagement/Controllers/ItemDoadoController.cs
+++ SaraiManagement/Controllers/ItemDoadoController.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SaraiManagement.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SaraiManagement.Controllers
 {
     public class ItemDoadoController : Controller
     {
+        private IItemDoadoRepositorio repositorio;
+
+        public ItemDoadoController(IItemDoadoRepositorio repo)
+        {
+            repositorio = repo;
+        }
+
         public IActionResult Index()
         {
             return View();

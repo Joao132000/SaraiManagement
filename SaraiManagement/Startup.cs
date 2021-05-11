35171f51c6debfa3d4e132ee1098347cf218bd8b
Davi_Nascimento <davi-ds15@hotmail.com>
2021-05-10 21:31:36 -0300

    Reload

--- SaraiManagement/Startup.cs
+++ SaraiManagement/Startup.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using SaraiManagement.Models.CassesEF;
+using SaraiManagement.Models.ClassesEF;
 using SaraiManagement.Models;
 using Microsoft.EntityFrameworkCore.SqlServer;
 using Microsoft.EntityFrameworkCore.Design;

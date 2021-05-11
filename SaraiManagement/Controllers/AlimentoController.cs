b941e7a6209aad50429508167dc7e612675fa775
Matheus Fidelis <matheusfidelis@yahoo.com>
2021-05-10 21:58:31 -0300

    Intanciamento resto de Controllers

---
+++ SaraiManagement/Controllers/AlimentoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SaraiManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SaraiManagement.Controllers
+{
+    public class AlimentoController : Controller
+    {
+        private IAlimentoRepositorio repositorio;
+
+        public AlimentoController(IAlimentoRepositorio repo)
+        {
+            repositorio = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SaraiManagement/Controllers/OutrosController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SaraiManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SaraiManagement.Controllers
+{
+    public class OutrosController : Controller
+    {
+        private IOutrosRepositorio repositorio;
+
+        public OutrosController(IOutrosRepositorio repo)
+        {
+            repositorio = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

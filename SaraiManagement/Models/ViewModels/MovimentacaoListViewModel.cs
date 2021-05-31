8e3dd9de6c4d7fb50fc09eb91748ac0ec44d4363
Joao Paulo Carvalho <71353908+Joao132000@users.noreply.github.com>
2021-05-31 18:56:57 -0300

    1212121212

--- SaraiManagement/Models/ViewModels/MovimentacaoListViewModel.cs
+++ SaraiManagement/Models/ViewModels/MovimentacaoListViewModel.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace SaraiManagement.Models.ViewModels
 {
     public class MovimentacaoListViewModel
     {
         public IEnumerable<Movimentacao> Movimentacaos { get; set; }
+        
+
+
         public PagingInfo PagingInfo { get; set; }
     }
 }

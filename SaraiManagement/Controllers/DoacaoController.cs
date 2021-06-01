c47ca65d11ece99c7aac47836b1a11f9731dd917
Matheus Fidelis <matheusfidelis@yahoo.com>
2021-05-31 23:34:30 -0300

    Atualizacao

--- SaraiManagement/Controllers/DoacaoController.cs
+++ SaraiManagement/Controllers/DoacaoController.cs
@@ -65,15 +65,10 @@ namespace SaraiManagement.Controllers
             var acesso = HttpContext.Session.GetString("usuario_session");
             if (acesso != null)
             {
-<<<<<<< HEAD
                 var a = HttpContext.Session.GetString("usuario_session1");
                 int i = int.Parse(a.ToString());
                 ViewBag.UsuarioID = new SelectList(context.Usuarios.Where(d => d.UsuarioID == i), "UsuarioID", "Nome");
-                ViewBag.DonatarioID = new SelectList(context.Donatarios.OrderBy(f => f.Nome), "DonatarioID", "Nome");
-=======
-                ViewBag.DonatarioID = new SelectList(context.Donatarios.Where(e => e.DonatarioID == idDonatario), "DonatarioID", "Nome");
-                ViewBag.UsuarioID = new SelectList(context.Usuarios.OrderBy(f => f.Nome), "UsuarioID", "Nome");
->>>>>>> JoaoPaulo
+                ViewBag.DonatarioID = new SelectList(context.Donatarios.Where(e => e.DonatarioID == idDonatario), "DonatarioID", "Nome");                
                 ViewBag.CaixaID = new SelectList(context.Caixas.OrderBy(f => f.Descricao), "CaixaID", "Descricao");
 
                 return View();
